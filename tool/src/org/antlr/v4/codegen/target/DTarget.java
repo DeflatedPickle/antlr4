540f8122cd4dc918f78c612d92e947115ec4c419
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-19 05:42:42 +0100

    Added D keywords

--- tool/src/org/antlr/v4/codegen/target/DTarget.java
+++ tool/src/org/antlr/v4/codegen/target/DTarget.java
@@ -5,6 +5,29 @@ import org.antlr.v4.codegen.Target;
 import org.antlr.v4.tool.ast.GrammarAST;
 
 public class DTarget extends Target {
+	protected static final String[] dKeywords = {"abstract", "alias", "align", "asm", "assert", "auto",
+		"body", "bool", "break", "byte",
+		"case", "cast", "catch", "cdouble", "cent", "cfloat", "char", "class", "const", "continue", "creal",
+		"dchar", "debug", "default", "delegate", "delete", "deprecated", "do", "double",
+		"else", "enum", "export", "extern",
+		"false", "final", "finally", "float", "for", "foreach", "foreach_reverse", "function",
+		"goto",
+		"idouble", "if", "ifloat", "immutable", "import", "in", "inout", "int", "interface", "invariant", "ireal", "is",
+		"lazy", "long",
+		"macro", "mixin", "module",
+		"new", "nothrow", "null",
+		"out", "override",
+		"package", "pragma", "private", "protected", "public", "pure",
+		"real", "ref", "return",
+		"scope", "shared", "short", "static", "struct", "super", "switch", "syncronized",
+		"template", "this", "throw", "true", "try", "typedef", "typeid", "typeof",
+		"ubyte", "ucent", "uint", "ulong", "union", "unittest", "ushort",
+		"version", "void",
+		"wchar", "while", "with",
+		"__FILE__", "__FILE_FULL_PATH__", "__MODULE__", "__LINE__", "__FUNCTION__", "__PRETTY_FUNCTION__",
+		"__gshared", "-_traits", "__vector", "__parameters"
+	};
+
 	protected DTarget(CodeGenerator gen) {
 		super(gen, "D");
 	}
